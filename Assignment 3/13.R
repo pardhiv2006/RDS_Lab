13c2da730b9eb36f50e94c62ca66c6fae76128eb
pardhiv2006 <pardhivmaddu@gmail.com>
2025-03-11 15:23:58 +0530

    Update 13.R

--- Assignment 3/13.R
+++ Assignment 3/13.R
@@ -1,1 +1,7 @@
+vector <- c("Jan", "Mar", "Feb", "Apr", "May", "Dec", "Nov") 
+month_levels <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+ordered factor <- factor(x = vector, levels = month_levels, ordered = TRUE)
+
+print(ordered factor)
 
