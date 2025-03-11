7ff9efcdff5a770681a7c5cc12bcaf56ec4c3dd6
pardhiv2006 <pardhivmaddu@gmail.com>
2025-03-11 15:12:57 +0530

    Update 04.R

--- Assignment 3/04.R
+++ Assignment 3/04.R
@@ -1,18 +1,12 @@
 logical_vector <- c(TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, FALSE)
 
-
-
 logical_matrix <- matrix(logical_vector, nrow=3, ncol=4)
 
 print(logical_matrix)
 
-
-
 numeric_matrix <- matrix(c(1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0), nrow=3, ncol=4)
 
 comparison_matrix <- logical_matrix == numeric_matrix
-
-
 
 print(comparison_matrix)
 
