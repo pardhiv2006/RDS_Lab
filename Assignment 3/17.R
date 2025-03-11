026c18b11f5e9b74374f3dfd08a2015a5dc1bf7f
pardhiv2006 <pardhivmaddu@gmail.com>
2025-03-11 15:31:26 +0530

    Update 17.R

--- Assignment 3/17.R
+++ Assignment 3/17.R
@@ -1,1 +1,11 @@
+vector1 <- c(TRUE, FALSE, TRUE, FALSE, TRUE)
+vector2 <- c(FALSE, FALSE, TRUE, TRUE, FALSE)
+
+logical_and <- vector1 & vector2
+logical_or <- vector1 | vector2
+logical_not <- !vector1
+
+print(logical_and) 
+print(logical_or) 
+print(logical_not) 
 
