82973c367e328e05dadfcb80945a9693a88883fd
pardhiv2006 <pardhivmaddu@gmail.com>
2025-03-11 15:20:36 +0530

    Update 11.R

--- Assignment 3/11.R
+++ Assignment 3/11.R
@@ -1,1 +1,8 @@
+text <- "apple, apple, and apple"
+
+first_replacement <- sub("apple", "orange", text)
+all_replacements <- gsub("apple", "orange", text)
+
+print(first_replacement)
+print(all_replacements)
 
