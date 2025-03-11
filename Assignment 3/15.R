f017a171e0ee388f93529b94a53fe5721162e3b1
pardhiv2006 <pardhivmaddu@gmail.com>
2025-03-11 15:26:52 +0530

    Update 15.R

--- Assignment 3/15.R
+++ Assignment 3/15.R
@@ -1,1 +1,8 @@
+numeric_vector <- c(5, 15, 25, 35, 45, 55, 65, 75, 85, 95)
+
+binned_vector <- cut(numeric_vector, 
+                     breaks = c(0, 30, 70, 100), 
+                     labels = c("Low", "Medium", "High"))
+
+print(binned_vector)
 
