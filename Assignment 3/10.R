acd6cf809223a636d6d63fdb57433b3fa2c0af25
pardhiv2006 <pardhivmaddu@gmail.com>
2025-03-11 15:19:29 +0530

    Update 10.R

--- Assignment 3/10.R
+++ Assignment 3/10.R
@@ -1,1 +1,10 @@
+word1 <- "Learning"
+word2 <- "Data"
+word3 <- "Science"
+word4 <- "in R"
+
+pasted_string <- paste(word1, word2, word3, word4, sep = " | ")
+print(pasted_string)
+
+cat(word1, word2, word3, word4, sep = " | ")
 
