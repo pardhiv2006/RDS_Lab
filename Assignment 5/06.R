aec3f88407bf9efbcd43aad8329acf2a077d3741
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:54:27 +0530

    Update 06.R

--- Assignment 5/06.R
+++ Assignment 5/06.R
@@ -1,1 +1,10 @@
+library(ggplot2)
 
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 1, 8, 7)
+
+ggplot(data = data.frame(x, y), aes(x = x, y = y)) +
+  geom_point(size = 4, shape = 17) + 
+  labs(title = "Customized Scatter Plot",
+       x = "X-axis",
+       y = "Y-axis")
