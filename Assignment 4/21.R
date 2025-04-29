9aaa7a68d3d7b9a4bd99909d2d20d68f4b913ef7
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:00:47 +0530

    Update 21.R

--- Assignment 4/21.R
+++ Assignment 4/21.R
@@ -1,1 +1,10 @@
+merge_lists <- function(list1, list2) {
+  list(list1, list2)
+}
 
+list1 <- list(a = 1, b = 2)
+list2 <- list(x = "Hello", y = "World")
+
+result <- merge_lists(list1, list2)
+
+print(result)
