2995738699f8ce1abab1663cd102b8baced2c0d6
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 16:17:51 +0530

    Update 09.R

--- Assignment 4/09.R
+++ Assignment 4/09.R
@@ -1,1 +1,11 @@
+name <- c("Rajesh", "Yogesh","Sonsay","Laxmi")
 
+age <- c(19,20,20,19)
+
+df <- data.frame(name,age)
+
+new_row <- data.frame(name = "Preeti", age = 22)
+
+df <- rbind(df, new_row)
+
+df
