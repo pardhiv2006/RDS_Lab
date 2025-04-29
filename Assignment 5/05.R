1d8c2d66143271620c419033de941c9ee3d870b7
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:33:04 +0530

    Update 05.R

--- Assignment 5/05.R
+++ Assignment 5/05.R
@@ -1,1 +1,17 @@
+library(ggplot2)
 
+
+df <- data.frame(
+  x = c(1, 2, 3, 4, 5),
+  y = c(3, 7, 4, 9, 6)
+)
+
+ggplot(data = df, aes(x = x, y = y)) +
+  geom_line()
+
+ggplot(data = df, aes(x = x, y = y)) +
+  geom_line(color = "blue", size = 1.2, linetype = "dashed") +
+  geom_point(color = "red", size = 3) +
+  labs(title = "Customized Line Plot",
+       x = "X-Axis Label",
+       y = "Y-Axis Label")
