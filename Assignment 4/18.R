679ae89451d264fc11243de573f3605800fbd388
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 17:47:14 +0530

    Update 18.R

--- Assignment 4/18.R
+++ Assignment 4/18.R
@@ -1,1 +1,10 @@
+df <- data.frame(
+  name = c("Rajesh","Yogesh","Sonsay","Laxmi","Preeti"),
+  age = c(19, 20, 20, 19, 22)
+)
 
+city = c("Dantewada","Jagdalpur","Raipur","Sukma","Jagdalpur")
+
+df <- cbind(df, city)
+
+df
