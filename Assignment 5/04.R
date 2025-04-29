f1dda32cab6465cca028e96cfafdcf0d26f31b43
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:32:03 +0530

    Update 04.R

--- Assignment 5/04.R
+++ Assignment 5/04.R
@@ -1,1 +1,13 @@
+library(ggplot2)
 
+df <- data.frame(
+  x = c(1, 2, 3, 4, 5),
+  y = c(3, 7, 4, 9, 6)
+)
+
+ggplot(data = df, aes(x = x, y = y)) +
+  geom_line()
+
+ggplot(data = df, aes(x = x, y = y)) +
+  geom_line(color = "blue", size = 1.2, linetype = "dashed") +
+  geom_point(color = "red", size = 3)
