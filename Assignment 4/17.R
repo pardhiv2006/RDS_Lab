e4cd1dea7befd39bcf55e466da07a0eebe3ca25f
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 16:24:48 +0530

    Update 17.R

--- Assignment 4/17.R
+++ Assignment 4/17.R
@@ -1,1 +1,9 @@
+df <- data.frame(
+  name = c("Rajesh", "Yogesh", "Sonsay", "Laxmi"),
+  age = c(20, 25, 22, 20),
+  city = c("Delhi", "Mumbai", "Kolkata", "Chennai")
+)
 
+subset_df <- df[1:3, 1:2]
+
+print(subset_df)
