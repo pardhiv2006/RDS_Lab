8f0716d1237e94fccd11e0c5c4c7ed9c521b385a
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:02:59 +0530

    Update 24.R

--- Assignment 4/24.R
+++ Assignment 4/24.R
@@ -1,1 +1,9 @@
+my_list <- list(
+  matrix_data = matrix(1:4, nrow = 2, ncol = 2),  
+  logical_vector = c(TRUE, FALSE, TRUE, TRUE),    
+  my_string = "Hello, World!"                 
+)
 
+second_logical_element <- my_list$logical_vector[2]
+
+print(second_logical_element)
