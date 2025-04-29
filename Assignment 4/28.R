67bd2952e996826f6374dd0d4ee6e2583fc9c1d1
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:08:46 +0530

    Update 28.R

--- Assignment 4/28.R
+++ Assignment 4/28.R
@@ -1,1 +1,15 @@
+nested_list <- list(
+  level1 = list(
+    level2 = list(
+      level3 = list(
+        level4 = list(
+          value = 42 
+        )
+      )
+    )
+  )
+)
 
+deep_value <- nested_list$level1$level2$level3$level4$value
+
+print(deep_value)
