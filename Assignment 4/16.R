44f8d57a26cf14b7041c96f957822b8695ed2425
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 16:24:16 +0530

    Update 16.R

--- Assignment 4/16.R
+++ Assignment 4/16.R
@@ -1,1 +1,16 @@
+my_list <- list(
+  numbers = c(1, 2, 3),
+  letters = c("A", "B", "C"),
+  logical = TRUE
+)
 
+# Single bracket [ ] slicing
+single_bracket <- my_list[1]   # This is still a list
+print(single_bracket)
+# Output: $numbers
+#         [1] 1 2 3
+
+# Double bracket [[ ]] referencing
+double_bracket <- my_list[[1]]  # This gives the actual vector
+print(double_bracket)
+# Output: [1] 1 2 3
