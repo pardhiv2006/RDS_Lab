3d0caf36d97e49a69408e22f266e06b3af13f458
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:55:21 +0530

    Update 07.R

--- Assignment 5/07.R
+++ Assignment 5/07.R
@@ -1,1 +1,17 @@
+# List All Files in the Current Working Directory
+list.files()
 
+#List All Files Including Hidden Files
+list.files(all.files = TRUE)
+
+#List Files Matching a Specific Pattern
+list.files(pattern = "\\.csv$")
+
+#List Files with Full Paths
+list.files(full.names = TRUE)
+
+#List Files Recursively in Subdirectories
+list.files(recursive = TRUE)
+
+#List Files in a Specific Directory
+list.files(path = "D:\R_Assignments\Assignment_5")
