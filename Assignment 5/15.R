ea9442c8cf5e53a40db033ae0facc2cde2b400e2
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 19:05:00 +0530

    Update 15.R

--- Assignment 5/15.R
+++ Assignment 5/15.R
@@ -1,1 +1,17 @@
+library(ggplot2)
 
+data <- data.frame(
+  x = c(1, 2, 3, 4, 5, 6),
+  y = c(2, 4, 1, 5, 7, 3),
+  group = c("A", "B", "A", "B", "A", "B")
+)
+
+ggplot(data, aes(x = x, y = y, color = group)) +
+  geom_point(size = 3) +
+
+  geom_text(aes(label = group), vjust = -1, color = "black") +
+
+  annotate("segment", x = 3, y = 1, xend = 5, yend = 7,
+           arrow = arrow(type = "closed", length = unit(0.2, "inches")), 
+           color = "red", size = 1.2) +
+  labs(title = "Scatter Plot with Text and Arrow Annotations")
