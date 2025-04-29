a1b349111f971ebf21d435e6d21df325cc521c34
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 19:06:06 +0530

    Update 17.R

--- Assignment 5/17.R
+++ Assignment 5/17.R
@@ -1,1 +1,14 @@
+library(ggplot2)
 
+data <- data.frame(
+  x = c(1, 2, 3, 4, 5, 6),
+  y = c(2, 4, 1, 5, 7, 3),
+  group = c("A", "B", "A", "B", "A", "B")
+)
+
+data$highlight <- ifelse(data$y > 4, "Highlight", "Normal")
+
+ggplot(data, aes(x = x, y = y, color = highlight)) +
+  geom_point(size = 3) +
+  scale_color_manual(values = c("Normal" = "blue", "Highlight" = "red")) + 
+  labs(title = "Scatter Plot with Highlighted Points")
