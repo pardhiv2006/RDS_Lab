b34a2247cb6660ed0e75b37895869e1c0c7b8fda
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:59:01 +0530

    Update 11.R

--- Assignment 5/11.R
+++ Assignment 5/11.R
@@ -1,1 +1,10 @@
+library(ggplot2)
 
+data <- data.frame(
+  x = c(1, 2, 3, 4, 5, 6),
+  y = c(2, 4, 1, 5, 7, 3),
+  group = c("A", "B", "A", "B", "A", "B")
+)
+
+ggplot(data, aes(x = x, y = y, color = group)) +
+  geom_point()
