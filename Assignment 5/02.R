966ab97986a52c2b36db46e7e9cbb28bc0803d76
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:30:41 +0530

    Update 02.R

--- Assignment 5/02.R
+++ Assignment 5/02.R
@@ -1,1 +1,12 @@
+library(ggplot2)
 
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 1, 8, 7)
+
+qplot(x, y)
+
+qplot(x, y,
+      main = "Scatter Plot of x vs y",
+      xlab = "X-axis Label",
+      ylab = "Y-axis Label",
+      color = I("blue"))
