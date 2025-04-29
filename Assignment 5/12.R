7601ca8cf45240de02b5cf8a0bbacda4ee8601d6
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 19:02:27 +0530

    Update 12.R

--- Assignment 5/12.R
+++ Assignment 5/12.R
@@ -1,1 +1,14 @@
+library(ggplot2)
 
+data <- data.frame(
+  x = c(1, 2, 3, 4, 5, 6),
+  y = c(2, 4, 1, 5, 7, 3),
+  group = c("A", "B", "A", "B", "A", "B")
+)
+
+p <- ggplot(data, aes(x = x, y = y, color = group)) +
+  geom_point()
+
+pdf("my_scatter_plot.pdf", width = 6, height = 4)  
+print(p)                                           
+dev.off()
