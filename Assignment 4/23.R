bd99756e0da1734bd471ce79dc812d5563e45dc1
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:02:18 +0530

    Update 23.R

--- Assignment 4/23.R
+++ Assignment 4/23.R
@@ -1,1 +1,19 @@
+list_data <- list(
+  name = c("Rajesh", "Yogesh", "Sonsay"),
+  age = c(20, 25),  # Shorter vector
+  city = c("Delhi", "Mumbai", "Kolkata", "Chennai") 
+)
 
+max_length <- max(sapply(list_data, length))
+
+pad_vector <- function(vec, max_length) {
+  length(vec) <- max_length 
+  return(vec) 
+}
+
+padded_list <- lapply(list_data, pad_vector, max_length = max_length)
+
+
+df <- data.frame(padded_list)
+
+print(df)
