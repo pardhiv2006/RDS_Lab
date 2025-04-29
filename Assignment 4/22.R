1ead769cc9bf90651d0f9471bc1a2bbbf1d4c9fe
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:01:26 +0530

    Update 22.R

--- Assignment 4/22.R
+++ Assignment 4/22.R
@@ -1,1 +1,11 @@
+names <- c("Rajesh", "Yogesh", "Sonsay", "Laxmi", "Sneha")
+ages <- c(20, 25)  # A shorter vector
+cities <- c("Delhi", "Mumbai", "Kolkata")  # Another shorter vector
 
+# Create a data frame
+df <- data.frame(name = names, 
+                 age = ages,  # This will recycle
+                 city = cities)  # This will recycle
+
+# Print the data frame
+print(df)
