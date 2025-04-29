787fc4c48f31d4fd0a303dd18e1757c13e70623b
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 19:15:46 +0530

    Update 28.R

--- Assignment 5/28.R
+++ Assignment 5/28.R
@@ -1,1 +1,14 @@
+save_plot_both_formats <- function(plot, filename, width = 8, height = 6, dpi = 300) {
 
+  jpeg_filename <- paste0(filename, ".jpg")
+  jpeg(jpeg_filename, width = width, height = height, units = "in", res = dpi)
+  print(plot)
+  dev.off()
+  
+  pdf_filename <- paste0(filename, ".pdf")
+  pdf(pdf_filename, width = width, height = height)
+  print(plot)
+  dev.off()
+  
+  cat("Plot saved as:", jpeg_filename, "and", pdf_filename, "\n")
+}
