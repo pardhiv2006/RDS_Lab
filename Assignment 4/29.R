5d2dfb34e3d2e9a3a2acb5fb773f3538deae83bf
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:09:20 +0530

    Update 29.R

--- Assignment 4/29.R
+++ Assignment 4/29.R
@@ -1,1 +1,21 @@
+add_factor_column <- function(df, numeric_column_name, factor_column_name) {
 
+  if (!numeric_column_name %in% colnames(df)) {
+    stop(paste("The column", numeric_column_name, "does not exist in the data frame"))
+  }
+  
+  df[[factor_column_name]] <- factor(df[[numeric_column_name]], 
+                                     levels = unique(df[[numeric_column_name]]))
+ 
+  return(df)
+}
+
+df <- data.frame(
+  id = 1:5,
+  age = c(20, 25, 30, 25, 30),
+  height = c(5.8, 6.1, 5.7, 5.5, 5.9)
+)
+
+df_updated <- add_factor_column(df, "age", "age_factor")
+
+print(df_updated)
