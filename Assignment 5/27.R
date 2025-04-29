b5f8c5645f859909fb7003b735e9cd75856ca4d8
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 19:14:51 +0530

    Update 27.R

--- Assignment 5/27.R
+++ Assignment 5/27.R
@@ -1,1 +1,17 @@
+library(ggplot2)
 
+set.seed(123)
+data <- data.frame(
+  x = rnorm(100),
+  y = rnorm(100)
+)
+
+data$group <- with(data, ifelse(x > 0 & y > 0, "Top Right",
+                                ifelse(x < 0 & y > 0, "Top Left",
+                                       ifelse(x < 0 & y < 0, "Bottom Left", "Bottom Right"))))
+
+ggplot(data, aes(x = x, y = y, color = group, shape = group)) +
+  geom_point(size = 3) +
+  labs(title = "Condition-Based Shapes and Colors",
+       x = "X Axis", y = "Y Axis", color = "Quadrant", shape = "Quadrant") +
+  theme_minimal()
