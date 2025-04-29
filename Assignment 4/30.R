e2f99623db8e6605d5ee109ca5a61f53d015f235
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:09:57 +0530

    Update 30.R

--- Assignment 4/30.R
+++ Assignment 4/30.R
@@ -1,1 +1,16 @@
+df <- data.frame(
+  id = 1:5,
+  age = c(22, 19, 25, 30, 18),
+  height = c(5.8, 5.4, 6.1, 5.6, 5.7),
+  city = c("Delhi", "Mumbai", "Mumbai", "Delhi", "Kolkata")
+)
 
+condition_age <- df$age > 20          
+condition_height <- df$height > 5.5   
+condition_city <- df$city %in% c("Delhi", "Mumbai")  
+
+combined_condition <- condition_age & condition_height & condition_city
+
+subset_df <- df[combined_condition, ]
+
+print(subset_df)
