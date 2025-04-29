d89d71b59d9bc98e1582242ccb1bbc75debc7a0d
pardhiv2006 <pardhivmaddu@gmail.com>
2025-04-29 18:04:09 +0530

    Update 25.R

--- Assignment 4/25.R
+++ Assignment 4/25.R
@@ -1,1 +1,18 @@
+my_list <- list(a = 1, b = 2)
 
+add_to_list <- function(list_obj) {
+
+  element_name <- readline(prompt = "Enter the name of the new element: ")
+  
+  element_value <- readline(prompt = paste("Enter the value for", element_name, ": "))
+  
+  element_value <- as.numeric(element_value)
+  
+  list_obj[[element_name]] <- element_value
+  
+  return(list_obj)
+}
+
+my_list <- add_to_list(my_list)
+
+print(my_list)
